1822721bc433a6a1c3fbea3847564251213a1ee1
Jean-Philippe Moffet <jeanphilippemoffet@gmail.com>
2022-07-25 18:56:16 -0400

    added offline

--- service-worker.js
+++ service-worker.js
@@ -14,12 +14,39 @@ self.addEventListener('install', (evt) => {
     );
     self.skipWaiting();
     });
+//Update cache names any time any of the cached files change.
+
+
 self.addEventListener('activate', (evt) => {
 console.log('[ServiceWorker] Activate');
 //Remove previous cached data from disk.
+evt.waitUntil(
+caches.keys().then((keyList) => {
+return Promise.all(keyList.map((key) => {
+if (key !== CACHE_NAME) {
+console.log('[ServiceWorker] Removing old cache',
+key);
+return caches.delete(key);
+}
+}));
+})
+);
 self.clients.claim();
 });
 self.addEventListener('fetch', (evt) => {
-console.log('[ServiceWorker] Fetch', evt.request.url);
-//Add fetch event handler here.
-});
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+    //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+    }
+    evt.respondWith(
+    fetch(evt.request)
+    .catch(() => {
+    return caches.open(CACHE_NAME)
+    .then((cache) => {
+    return cache.match('offline.html' );
+    });
+    })
+    );
+    });
